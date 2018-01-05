dc9ecea6dbf0d380e29c7e691dcfe452922b3732
Matt <wrestler.411@comcast.net>
2018-01-05 10:42:01 -0800

    Put in addBefore and addAfter

--- doubly-linked-lists/Method.cs
+++ doubly-linked-lists/Method.cs
@@ -12,7 +12,113 @@ namespace Methods
         public Methods(Node node)
         {
             Head = node;
+
             Counter = 1;
         }
+
+        
+        public void AddFirst(Node first)
+        {
+            if (Head == null)
+            {
+                Head = first;
+            }
+            else
+            {
+                first.Next = Head;
+
+                Head = first;
+
+                Head.Next.Prev = Head;
+            }
+
+            Counter++;
+        }
+
+        public void AddLast(Node last)
+        {
+            if (Head == null)
+            {
+                Head = last;
+            }
+            else
+            {
+                Node Current = Head;
+
+                while (Current.Next != null)
+                {
+                    Current = Current.Next;
+                }
+
+                Current.Next = last;
+
+                Current.Next.Prev = Current;
+            }
+            Counter++;
+        }
+
+        public void AddBefore(Node targetNode, Node newNode)
+        {
+            if (Head == null)
+            {
+                return;
+            }
+
+            Node Current = Head;
+
+            while (Current.Next != targetNode)
+            {
+                if (Current.Next != null)
+                {
+                    Current = Current.Next;
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            newNode.Next = Current.Next;
+
+            Current.Next.Prev = newNode;
+
+            Current.Next = newNode;
+
+            newNode.Prev = Current;
+
+            Counter++;
+        }
+
+        public void AddAfter(Node targetNode, Node newNode)
+        {
+            if (Head == null)
+            {
+                return;
+            }
+
+            Node Current = Head;
+
+            while (Current != targetNode)
+            {
+                if (Current.Next != null)
+                {
+                    Current = Current.Next;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            newNode.Next = Current.Next;
+
+            Current.Next.Prev = newNode;
+
+            Current.Next = newNode;
+
+            newNode.Prev = Current;
+
+            Counter++;
+        }
+
     }
 }

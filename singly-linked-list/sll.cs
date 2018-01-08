830f06594c17a02c4167d058c5046d3f8e5dceef
Matt <wrestler.411@comcast.net>
2018-01-07 23:43:48 -0800

    made sllnode.cs and Addfirst method

---
+++ singly-linked-list/sll.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeChallenges.Week_02
+{
+    public class SLL
+    {
+        public SLLNode Head = new SLLNode();
+
+        public void AddFirst(int value)
+        {
+            SLLNode newNode = new SLLNode { Value = value };
+            if (Head == null)
+            {
+                Head = newNode;
+                return;
+            }
+            newNode.Next = Head;
+            Head = newNode;
+            return;
+        }
---
+++ singly-linked-list/sllnode.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeChallenges.Week_02
+{
+    public class SLLNode
+    {
+        public int Value;
+        public SLLNode Next;
+        public SLLNode Prev;
+    }
+}
